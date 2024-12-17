245d187a535f80ff1b28534bc786f7e53c719a7c
Admin <Admin@WIN-0QFN66U68EP>
2024-12-17 04:25:36 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите предложение, завершающееся точкой:");
+        string input = Console.ReadLine();
+
+        // Убираем точку в конце для удобства обработки
+        if (input.EndsWith("."))
+        {
+            input = input.TrimEnd('.');
+        }
+
+        Dictionary<char, int> counts = new Dictionary<char, int>();
+
+        // Подсчет символов
+        foreach (char c in input)
+        {
+            if (counts.ContainsKey(c))
+            {
+                counts[c]++;
+            }
+            else
+            {
+                counts[c] = 1;
+            }
+        }
+
+        Console.WriteLine("Символы, которые встречаются ровно один раз:");
+        foreach (var pair in counts)
+        {
+            if (pair.Value == 1)
+            {
+                Console.Write(pair.Key + " ");
+            }
+        }
+    }
+}
